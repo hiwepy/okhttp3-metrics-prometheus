cfd244940a337033c893e39863976a33dbee4e24
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 03:09:18 +0800

    sync: cherry-pick changes from feature/3.0.x

--- src/test/java/okhttp3/metrics/OkHttp3MetricsTests.java
+++ src/test/java/okhttp3/metrics/OkHttp3MetricsTests.java
@@ -30,7 +30,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
